ae3a8569ca0c53bbf3e17366e97bdefb9ceedc2f
Eliud Niebles Lopez <eliudniebles@gmail.com>
2024-08-20 11:59:06 -0500

    plugins

---
+++ config/plugins/defaulf.nix
@@ -0,0 +1,6 @@
+{
+  imports = [
+  ./treeseter
+  ./cmp
+  ];
+}
---
+++ config/plugins/treeseter/default.nix
@@ -0,0 +1,11 @@
+{
+  plugins ={
+    treestter = {
+      enable = true;
+
+      settings {
+        ensure_installed = "all";
+      }
+    };
+  };
+}
