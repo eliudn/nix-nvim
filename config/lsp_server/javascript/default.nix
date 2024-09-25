7be5acc9f76071821d99f597946aa01d897a10e1
Eliud Niebles Lopez <eliudniebles@gmail.com>
2024-09-25 10:25:53 -0500

    Configuracion de error de typescript

--- config/lsp_server/javascript/default.nix
+++ config/lsp_server/javascript/default.nix
@@ -9,23 +9,22 @@
           "javascript"
           "javascriptreact"
           "typescriptreact"
-          "vue"
         ];
-        extraOptions = {
-          init_options = {
-            plugins = [
-              {
-                name = "@vue/typescript-plugin";
-                location = "${lib.getBin pkgs.vue-language-server}/lib/node_modules/@vue/language-server";
-                languages = [
-                  "typescript"
-                  "vue"
-                ];
-              }
-            ];
-
-          };
-        };
+        # extraOptions = {
+        #   init_options = {
+        #     plugins = [
+        #       {
+        #         name = "@vue/typescript-plugin";
+        #         location = "${lib.getBin pkgs.vue-language-server}/lib/node_modules/@vue/language-server";
+        #         languages = [
+        #           "typescript"
+        #           "vue"
+        #         ];
+        #       }
+        #     ];
+        #
+        #   };
+        # };
       };
       volar = {
         enable = true;
--- config/plugins/ia/avante.nix
+++ config/plugins/ia/avante.nix
@@ -5,8 +5,16 @@
   ];
 
   extraConfigLua = ''
-    require'avante'.setup({
+    require('avante_lib').load()
+    require('avante').setup({
       provider = "claude",
+      auto_suggentions_provider = "claude",
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-5-sonnet-20240620",
+        temperature = 0,
+        max_tokens = 4096,
+      }
     })
   '';
 }
